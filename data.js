1a6b96ed8a72f7871c776c493fcc1d6788d615d9
diegoram23 <diego.pta.oc@gmail.com>
2022-12-16 18:14:51 -0800

    Added current order styles

--- data.js
+++ data.js
@@ -1,20 +1,20 @@
 export const menuArray = [
     {
-        name: "Taco",
+        name: "Three Tacos",
         ingredients: ["carne asada", " salsa", " limon"],
         price: 14,
         emoji: "🌮",
         id: '0'
     },
     {
-        name: "Burrito",
+        name: "Burrito Grande",
         ingredients: ["ground beef", " cheese", " frijoles"],
         price: 12,
         emoji: "🌯",
         id: '1'
     },
         {
-        name: "Corona",
+        name: "Corona Fresca",
         ingredients: ["grain, hops, yeast, water"],
         price: 12,
         emoji: "🍺",
